aa976cc23fc5e00ad5c704220e0b95abe681f54f
Hanul <hanul@hanul.me>
2022-02-16 18:04:15 +0900

    구현 완료

--- src/contracts/MinterContract.ts
+++ src/contracts/MinterContract.ts
@@ -1,4 +1,5 @@
 import { BigNumber } from "@ethersproject/bignumber";
+import Alert from "../component/dialogue/Alert";
 import Config from "../Config";
 import ExtWallet from "../klaytn/ExtWallet";
 import KlubsContentsNFTMinterArtifact from "./abi/artifacts/contracts/KlubsContentsNFTMinter.sol/KlubsContentsNFTMinter.json";
@@ -21,12 +22,12 @@ class MinterContract extends Contract {
     public async mint(count: number): Promise<void> {
         const amount = (await this.amount()).toNumber();
         if (count > amount) {
-            alert(`남은 개수는 ${amount}개입니다.`);
+            new Alert("오류", `남은 개수는 ${amount}개입니다.`);
         } else {
             const price = (await this.price()).mul(count);
             const balance = await ExtWallet.loadBalance();
             if (balance === undefined || balance.lt(price)) {
-                alert("Klay가 부족합니다.");
+                new Alert("오류", "Klay가 부족합니다.");
             } else {
                 await this.runWalletMethodWithValue(price, "mint", count);
             }
